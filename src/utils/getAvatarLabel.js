34e4fcc63d7d964c5c4882c6520fd5c50e55e355
CKanishka <ckanishka24@gmail.com>
2021-08-03 19:54:44 +0530

    Removed redundant files

--- src/utils/getAvatarLabel.js
+++
@@ -1,9 +0,0 @@
-export const getAvatarLabel = (name) => {
-    if(name && name.trim()){
-        const nameParts = name.split(' ');
-        return nameParts.reduce((prev,curr)=>`${prev}${curr[0]?curr[0].toUpperCase():''}`,'')
-    }
-    else {
-        return 'U'
-    }
-}
